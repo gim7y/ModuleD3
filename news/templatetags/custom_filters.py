237deafa115aa1418cd9f9e847215d0b8e653544
marika giga <marikgiga12@gmail.com>
2023-04-10 23:06:17 +0300

    This is a third version of project News

---
+++ news/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='censor')
+def censor(val, arg):
+    bannedList = ['идиот', 'придурок', 'черт', 'козел']
+    text = val
+    result = ''
+
+    for word in bannedList:
+        data = text.lower().replace(word, arg * len(word))
+        text = data
+
+    for i in range(len(val)):
+        if val[i] != text[i]:
+            result += text[i].upper()
+        else:
+            result += text[i]
+
+    return result
---
+++ news/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    queryset = Post.objects.order_by('-dateCreation')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['value1'] = None
+        return context
+
+# создаём представление, в котором будут детали конкретного отдельного товара
+class PostDetail(DetailView):
+    model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'post.html'  # название шаблона будет product.html
+    context_object_name = 'post'  # название объекта
